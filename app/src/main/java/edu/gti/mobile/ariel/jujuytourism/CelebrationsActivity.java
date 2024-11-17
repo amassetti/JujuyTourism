1569918e972bd86c8fe39fe34f0f9afdd8e50823
Ariel Massetti <ariel@finconecta.com>
2024-11-17 13:49:45 +0000

    Adding justification in What to Do page

--- app/src/main/java/edu/gti/mobile/ariel/jujuytourism/CelebrationsActivity.java
+++ app/src/main/java/edu/gti/mobile/ariel/jujuytourism/CelebrationsActivity.java
@@ -1,6 +1,9 @@
 package edu.gti.mobile.ariel.jujuytourism;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,6 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CelebrationsActivity extends AppCompatActivity {
+
+    private static final String URL_ERMITAS = "https://www.turismo.jujuy.gob.ar/event/ermitas-de-tilcara/";
+    private static final String URL_WHITE_RIVER = "https://www.turismo.jujuy.gob.ar/event/fiesta-patronal-y-peregrinacion-a-la-virgen-de-rio-blanco-y-paypaya/";
+    private static final String URL_NAUTIC = "https://www.turismo.jujuy.gob.ar/event/la-patrona-de-el-carmen-en-peregrinacion-nautica/";
+    private static final String URL_MANGERS = "https://www.turismo.jujuy.gob.ar/event/tradicion-de-pesebres-y-adoracion-al-nino-dios/";
 
     ImageView imageViewMangersJ, imageViewWhiteRiverJ, imageViewNauticJ, imageViewErmitasJ;
 
@@ -25,7 +33,45 @@ public class CelebrationsActivity extends AppCompatActivity {
         imageViewNauticJ = findViewById(R.id.imageViewNautic);
         imageViewErmitasJ = findViewById(R.id.imageViewErmitas);
 
+        // Ermitas
+        imageViewErmitasJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URL_ERMITAS));
+                startActivity(browserIntent);
 
+            }
+        });
+
+        // Nautic
+        imageViewNauticJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URL_NAUTIC));
+                startActivity(browserIntent);
+
+            }
+        });
+
+        // White River
+        imageViewWhiteRiverJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URL_WHITE_RIVER));
+                startActivity(browserIntent);
+
+            }
+        });
+
+        // Mangers
+        imageViewMangersJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(URL_MANGERS));
+                startActivity(browserIntent);
+
+            }
+        });
 
         /* */
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
